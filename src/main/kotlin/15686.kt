72f4bdb31d20d91921b9d300facbf83395cdae8d
grusie <rbtjr02@naver.com>
2023-01-26 21:12:45 +0900

    23.01.26

--- src/main/kotlin/15686.kt
+++ src/main/kotlin/15686.kt
@@ -2,57 +2,69 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.StringTokenizer
+import java.util.*
 
+private lateinit var chicken: ArrayList<Pair<Int, Int>>
+private lateinit var home: ArrayList<Pair<Int, Int>>
+private lateinit var visit: Array<Boolean>
+private var ans = Int.MAX_VALUE
+private var open = 0
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     var st = StringTokenizer(br.readLine())
 
     val size = st.nextToken().toInt()
-    val open = st.nextToken().toInt()
-    val house = ArrayList<Pair<Int, Int>>()
-    val chicken = ArrayList<Pair<Int, Int>>()
-    var ans = Int.MAX_VALUE
+    open = st.nextToken().toInt()
 
-    fun dfs(start: Int, count: Int) {
-        val visit = Array(chicken.size) { false }
-        if (count == open) {
-            var result = 0
-            for(i in house){
-                var temp = Int.MAX_VALUE
-                for(j in 0 until chicken.size) {
-                    if(visit[j]) {
-                        var dist = kotlin.math.abs(i.first - chicken[j].first) + kotlin.math.abs(i.second - chicken[j].second)
-                        temp = dist.coerceAtMost(temp)
-                    }
-                }
-                result += temp
-            }
-            ans = result.coerceAtMost(ans)
-            return
-        }
-        for (i in start until chicken.size) {
-            visit[i] = true
-            dfs(start + 1, count + 1)
-            visit[i] = false
-        }
-    }
+    chicken = ArrayList()
+    home = ArrayList()
 
     for (i in 0 until size) {
         st = StringTokenizer(br.readLine())
         for (j in 0 until size) {
-            var data = st.nextToken().toInt()
-            if (data == 1)
-                house.add(Pair(i, j))
-            else if (data == 2)
+            var data = st.nextToken()
+            if (data == "2")
                 chicken.add(Pair(i, j))
+            else if (data == "1")
+                home.add(Pair(i, j))
         }
     }
 
+    visit = Array(chicken.size) { false }
     dfs(0, 0)
     br.close()
     bw.write("$ans")
     bw.flush()
     bw.close()
+}
+
+private fun dfs(start: Int, depth: Int) {
+    if (depth == open) {
+        var result = 0
+        for (i in home) {
+            var temp = Int.MAX_VALUE
+            for (j in 0 until chicken.size) {
+                if (visit[j]) {
+                    if (temp == 1)
+                        break
+                    var dist = dist(i, chicken[j])
+                    temp = dist.coerceAtMost(temp)
+                }
+            }
+            result += temp
+        }
+        ans = result.coerceAtMost(ans)
+        return
+    }
+
+    for (i in start until chicken.size) {
+        visit[i] = true
+        dfs(i + 1, depth + 1)
+        visit[i] = false
+    }
+}
+
+private fun dist(homePoint: Pair<Int, Int>, chickenPoint: Pair<Int, Int>): Int {
+    return kotlin.math.abs(homePoint.first - chickenPoint.first) + kotlin.math.abs(homePoint.second - chickenPoint.second)
 }
